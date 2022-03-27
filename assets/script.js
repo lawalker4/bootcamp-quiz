484fce8804134281d510a2d95c848780fcc8848a
LA WALKER <lauraashleywalker4@gmail.com>
2022-03-27 16:12:04 -0400

    updated timer

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,54 @@
-var timerEl=document.querySelector("#timer")
-var introEl=document.querySelector("#intro")
-var startButtonEl=document.querySelector(".start-button")
-var questionsEl=document.querySelector("#questions")
-var initialEl=document.querySelector("#initial")
-var highScoreEl=document.querySelector("#high-score")
-var timeRemaining=5
-var clockId=null
+var timerEl = document.querySelector("#timer")
+var introEl = document.querySelector("#intro")
+var startButtonEl = document.querySelector(".start-button")
+var questionsEl = document.querySelector("#questions")
+var initialEl = document.querySelector("#initial")
+var highScoreEl = document.querySelector("#high-score")
+var timeRemaining = 75
+var clockId = null
 
 
-var questionTitelEl=document.querySelector("#question-title")
-var choice1El=document.querySelector("#Choice1")
-var choice2El=document.querySelector("#Choice2")
-var choice3E1=document.querySelector("#Choice3")
-var choice4E1=document.querySelector("#Choice4")
-var nameEl=document.querySelector("#name")
-var saveButtonEl=document.querySelector("#save")
+var questionTitelEl = document.querySelector("#question-title")
+var choice1El = document.querySelector("#Choice1")
+var choice2El = document.querySelector("#Choice2")
+var choice3E1 = document.querySelector("#Choice3")
+var choice4E1 = document.querySelector("#Choice4")
+var nameEl = document.querySelector("#name")
+var saveButtonEl = document.querySelector("#save")
 
 
 // add questions for quiz with their answers
 // make them clickable
-var questionarry=[
+var questionarry = [
     {
-    questionTitle:"Commonly used data types DO NOT include",
-    choices:["1. Strings","2. Booleans", "3. Alerts", "4.Numbers"],
-    answer:"4.",
+        questionTitle: "Commonly used data types DO NOT include",
+        choices: ["1. Strings", "2. Booleans", "3. Alerts", "4.Numbers"],
+        answer: "4.",
 
-},
-{
-    questionTitle:"The condition in an if/else statement is enclosed with ______________.",
-    choices:["1. Quotes","2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
-    answer:"3.",
+    },
+    {
+        questionTitle: "The condition in an if/else statement is enclosed with ______________.",
+        choices: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
+        answer: "3.",
 
-},
-{
-    questionTitle:"Arrys in JavaScript can be used to store _____________________.",
-    choices:["1. Numbers and Strings","2. Other Arrays", "3. Boolenans", "4. All the above"],
-    answer:"4.",
+    },
+    {
+        questionTitle: "Arrys in JavaScript can be used to store _____________________.",
+        choices: ["1. Numbers and Strings", "2. Other Arrays", "3. Boolenans", "4. All the above"],
+        answer: "4.",
 
-},
-{
-    questionTitle:"A very useful tool used during development and debuggging for printing content to the debugger is:",
-    choices:["1. JavaScript","2. Termnal/Bash", "3. For Loops", "4. Console.log"],
-    answer:"4.",
+    },
+    {
+        questionTitle: "A very useful tool used during development and debuggging for printing content to the debugger is:",
+        choices: ["1. JavaScript", "2. Termnal/Bash", "3. For Loops", "4. Console.log"],
+        answer: "4.",
 
-},
+    },
 
 
 ]
 
-var index=0
+var index = 0
 
 var scorearry = []
 
@@ -57,12 +57,12 @@ function selectAnswer() {
 }
 
 //Be able to click the start button
-startButtonEl.addEventListener("click",function(){
+startButtonEl.addEventListener("click", function () {
     introEl.classList.add("hide");
     questionsEl.classList.remove("hide");
     showQuestion()
-    clockId=setInterval(countdown,1000);
-    
+    clockId = setInterval(countdown, 1000);
+
 })
 
 choice1El.addEventListener("click", nextQuestion)
@@ -70,29 +70,29 @@ choice2El.addEventListener("click", nextQuestion)
 choice3E1.addEventListener("click", nextQuestion)
 choice4E1.addEventListener('click', nextQuestion)
 
-function nextQuestion () {
+function nextQuestion() {
     index++
-    if(index<questionarry.length){
+    if (index < questionarry.length) {
         showQuestion()
-    }else {
+    } else {
         clearInterval(clockId)
         initialEl.classList.remove("hide")
         questionsEl.classList.add("hide")
     }
 }
 
-function showQuestion () {
-    questionTitelEl.textContent=questionarry[index].questionTitle
-    choice1El.textContent=questionarry[index].choices[0]
-    choice2El.textContent=questionarry[index].choices[1]
-    choice3E1.textContent=questionarry[index].choices[2]
-    choice4E1.textContent=questionarry[index].choices[3]
+function showQuestion() {
+    questionTitelEl.textContent = questionarry[index].questionTitle
+    choice1El.textContent = questionarry[index].choices[0]
+    choice2El.textContent = questionarry[index].choices[1]
+    choice3E1.textContent = questionarry[index].choices[2]
+    choice4E1.textContent = questionarry[index].choices[3]
 }
 
-saveButtonEl.addEventListener("click",function(){
-    var name=nameEl.value 
-    var score=timeRemaining
-    scorearry.push({name,score})
+saveButtonEl.addEventListener("click", function () {
+    var name = nameEl.value
+    var score = timeRemaining
+    scorearry.push({ name, score })
     localStorage.setItem("score", JSON.stringify(scorearry))
     initialEl.classList.add("hide")
     highScoreEl.classList.remove("hide")
@@ -101,12 +101,21 @@ saveButtonEl.addEventListener("click",function(){
 
 //Be able to see the countdown
 
-function countdown(){
-    timerEl.innerHTML=timeRemaining;
-    timeRemaining=timeRemaining-1;
-    if (timeRemaining === 0){
+function countdown() {
+    timeRemaining = timeRemaining - 1;
+    timerEl.innerHTML = timeRemaining;
+    if (timeRemaining == 0) {
         console.log
-        clearInterval(timerEl)
+        clearInterval(clockId)
+        alert("Time is up!")
     }
-};
+} 1000;
+
+document.getElementById('incorrect').addEventListener('click', function () {
+    sec -= 5;
+    document.getElementById('timerDisplay').innerHTML = '00:' + sec;
+});
+
+
+
 
